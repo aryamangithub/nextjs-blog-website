11172cd52133393dfa1f3c9e729b378afc165064
aryamangithub <singharyaman357@gmail.com>
2025-02-05 20:03:14 +0530

    feat: Implement categories management with list and creation flow

--- app/admin/categories/page.jsx
+++ app/admin/categories/page.jsx
@@ -1,18 +1,20 @@
 import { CirclePlus } from "lucide-react";
 import Link from "next/link";
+import CategoriesListView from "./components/CategoriesListView";
 
 export default function Page(){
     return(
-        <main className="p-6 w-full">
+        <main className="p-6 w-full flex flex-col gap-6">
             <div className="flex justify-between items-center">
                 <h1 className="font-bold">Categories</h1>
                 <Link href="/admin/categories/form">
-                    <button className="flex gap-2 items-center bg-black px-4 py-2 text-white rounded-full font-bold">
-                        <CirclePlus/> 
-                        Add
+                    <button className="flex gap-2 items-center bg-black px-4 py-2 text-white rounded-full font-bold hover:bg-slate-900">
+                        <CirclePlus className="size-4"/> 
+                        Create
                     </button>
                 </Link>
             </div>
+            <CategoriesListView />
         </main>
     )
 }
