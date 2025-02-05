410e9175b4d6a49b06c5b1eefc3c4609c9097abc
aryamangithub <singharyaman357@gmail.com>
2025-02-05 20:01:51 +0530

    feat: Add category form with create, update, and delete functionality

--- app/admin/categories/form/page.jsx
+++ app/admin/categories/form/page.jsx
@@ -1,28 +1,56 @@
 "use client"
-
+import { useSearchParams } from "next/navigation"
 import { useCategoryForm } from "./contexts/CategoryFormContext"
+import { useEffect } from "react"
 
 export default function Page(){
+    const searchParams = useSearchParams()
+    const updateCategoryId = searchParams.get('id')
     const {
         data,
         isLoading,
         error,
         isDone,
-        image,
-        setImage,
         handleData,
         handleCreate,
+        handleUpdate,
+        handleDelete,
+        image,
+        setImage,
+        fetchData,
     } = useCategoryForm()
+    
+    useEffect(() => {
+      if(updateCategoryId){
+        fetchData(updateCategoryId)
+      }
+    }, [updateCategoryId])
 
+    const isLoadingStyle = "bg-gray-500 hover:bg-gray-600"
+    const createStyle = "bg-green-500 hover:bg-green-600"
+    const deleteStyle = "bg-red-500 hover:bg-red-600"
+    const updateStyle = "bg-blue-500 hover:bg-blue-600"
+    
     return(
         <main className="w-full flex flex-col gap-3 p-6">
-            <h1 className="font-bold">Categories | Form</h1>
+            <div className="flex gap-6">
+                {updateCategoryId && 
+                    <div>
+                        <h3 className="font-semibold text-blue-500">Update</h3>
+                    </div>
+                }
+                <h1 className="font-bold">Categories | Form</h1>
+            </div>
             <section className="flex">
                 <form 
                     className="flex flex-col gap-2 border shadow-sm rounded-xl p-7"
                     onSubmit={(e)=>{
                         e.preventDefault()
-                        handleCreate()
+                        if(updateCategoryId){
+                            handleUpdate()
+                        } else{
+                            handleCreate()
+                        }
                     }}
                 >
                     <div className="flex flex-col gap-2">
@@ -64,11 +92,16 @@ export default function Page(){
                             required
                         />
                     </div>
-                    {image && <div>
-                            <img className="h-40"src={URL.createObjectURL(image)} alt="" />
+                    <div className="flex">
+                        {data?.iconURL && <div>
+                                <img className="h-32"src={data?.iconURL} alt="" />
+                            </div>}
+                        {image && <div>
+                            <img className="h-32"src={URL.createObjectURL(image)} alt="" />
                         </div>}
+                    </div>
                     <div className="flex flex-col gap-2">
-                        <label className="text-sm font-medium text-black">Image <span className="text-red-500">*</span></label>
+                        <label className="text-sm font-medium text-black">Image</label>
                         <input
                             className="px-4 py-2 rounded-lg border" 
                             placeholder="Enter Category Decription"
@@ -78,17 +111,37 @@ export default function Page(){
                                 e.preventDefault()
                                 setImage(e.target.files[0])
                             }}
-                            required
                         />
                     </div>
+
                     {error && <p className="text-red-500 text-sm">{error}</p>}
-                    {!isDone && <button
-                        type="submit" 
-                        disabled={isLoading || isDone}
-                        className="bg-black rounded-lg px-4 py-2 text-white">
-                        {isLoading ? "Loading..." : "Create"}
-                    </button>}
-                    {isDone && <h3 className="text-green-500 text-center">Successfully created!</h3>}
+                    
+                    {!isDone && 
+                        <button
+                            type="submit" 
+                            disabled={isLoading || isDone}
+                            className= {`${isLoading ? isLoadingStyle : updateCategoryId ? updateStyle : createStyle} rounded-lg px-4 py-2 text-white`}>
+                            {isLoading ? "Loading..." : updateCategoryId ? "Update" : "Create"}
+                        </button>
+                    }
+
+                    {updateCategoryId && !isDone && 
+                        <button
+                            onClick={(e) => {
+                                e.preventDefault()
+                                handleDelete(updateCategoryId)
+                            }} 
+                            disabled={isLoading || isDone}
+                            className={`${isLoading ? isLoadingStyle : deleteStyle} rounded-lg px-4 py-2 text-white`}>
+                            {isLoading ? "Loading..." : "Delete"}
+                        </button>
+                    }
+
+                    {isDone && 
+                        <h3 className="text-green-500 text-center">
+                            Successfully { updateCategoryId ? "Updated" : "Created" }!
+                        </h3>
+                    }
                 </form>
             </section>
         </main>
