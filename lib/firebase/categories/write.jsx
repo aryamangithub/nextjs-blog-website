eb6077912adce2eaf8896a5997d069410a1872ab
aryamangithub <singharyaman357@gmail.com>
2025-02-05 19:53:23 +0530

    feat: Implement update and delete functionality for categories in Firestore

--- lib/firebase/categories/write.jsx
+++ lib/firebase/categories/write.jsx
@@ -1,7 +1,7 @@
 //Create new categories
 
 import { db, storage } from "@/lib/firebase"
-import { doc, setDoc, Timestamp } from "firebase/firestore"
+import { deleteDoc, doc, setDoc, Timestamp, updateDoc } from "firebase/firestore"
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
 
 export const createNewCategory = async({data, image}) => {
@@ -29,4 +29,38 @@ export const createNewCategory = async({data, image}) => {
         iconURL: imageURL,
         timestamp: Timestamp.now(),
     })
+}
+
+export const updateCategory = async({data, image}) => {
+    if(!data?.name){
+        throw new Error("Name undefined")
+    }
+    if(!data?.slug){
+        throw new Error("Slug undefined")
+    }
+    if(!data?.description){
+        throw new Error("Decription undefined")
+    }
+
+    var imageURL = data?.iconURL
+    if(image){
+        const imageRef = ref(storage, `categories/${data?.slug}.png`)
+        await uploadBytes(imageRef, image)
+        imageURL = await getDownloadURL(imageRef)        
+    }
+
+
+    const firestoreRef = doc(db, `categories/${data?.id}`)
+    await updateDoc(firestoreRef, {
+        ...data,
+        iconURL: imageURL,
+        timestamp: Timestamp.now(),
+    })
+}
+export const deleteCategory = async(id) => {
+    if(!id){
+        throw new Error("Id is required")
+    }
+
+    await deleteDoc(doc(db, `categories/${id}`))
 }
