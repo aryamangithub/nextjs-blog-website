5ab222d58867465417e976a4e60cd27d255e6fe0
aryamangithub <singharyaman357@gmail.com>
2025-02-10 15:02:04 +0530

    feat: wrap application with PostFormContextProvider in Layout component

---
+++ app/admin/posts/form/layout.jsx
@@ -0,0 +1,7 @@
+import PostFormContextProvider from "./contexts/PostFormContext";
+
+export default function Layout({children}) {
+    return <PostFormContextProvider>
+        {children}
+        </PostFormContextProvider>
+}
